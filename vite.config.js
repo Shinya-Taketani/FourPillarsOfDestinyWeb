04868ad9b2d1ac1dda0ebae32e36c83860892820
Shinya <shinya@localhost.localdomain>
2026-05-02 07:44:19 +0900

    vit adjust

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: '/FourPillarsOfDestinyWeb/public/build/',
     plugins: [
         laravel({
             input: 'resources/js/app.ts',
